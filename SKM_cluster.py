46e33c7d660f7847688e0f3fbb0747c339e019d0
jelman <jelman@lbl.gov>
2013-05-01 15:52:26 -0700

    First fully functional SKM_cluster.py script

--- SKM_cluster.py
+++ SKM_cluster.py
@@ -68,8 +68,8 @@ def skm_permute(data):
     Infile:
     ---------------
     data: nxp dataframe where n is observations and p is features (i.e. ROIs)
-            Should be a pandas DataFrame with subject codes as index and only
-            features to be used in clustering as columns.
+            Should be a pandas DataFrame with subject codes as index and features 
+            as columns.
     
     Returns:
     ---------------
@@ -104,8 +104,8 @@ def skm_cluster(data, L1bound):
     Infile:
     ---------------
     data: nxp dataframe where n is observations and p is features (i.e. ROIs)
-            Should be a pandas DataFrame with subject codes as index and only
-            features to be used in clustering as columns.
+            Should be a pandas DataFrame with subject codes as index and features 
+            as columns.
     
     Returns:
     ---------------
@@ -134,7 +134,6 @@ def skm_cluster(data, L1bound):
         
 def calc_cutoffs(data, weights, clusters):
     """
-    EDIT DOC STRING
     Determines the features with the top 50% weights used in clustering and determines 
     the cluster centers for these regions. Clusters are then labelled as positive or
     negative based on which has the highest average PIB index across these features. 
@@ -143,9 +142,9 @@ def calc_cutoffs(data, weights, clusters):
     
     Inputs
     ----------
-    data:   nxp dataframe where n is observations and p is features (i.e. ROIs)
-            Should be a pandas DataFrame with subject codes as index and only
-            features to be used in clustering as columns.
+    data: nxp dataframe where n is observations and p is features (i.e. ROIs)
+            Should be a pandas DataFrame with subject codes as index and features 
+            as columns.
             
     weights:    pandas DataFrame   
                 index = feature labels, values = weights
@@ -165,9 +164,9 @@ def calc_cutoffs(data, weights, clusters):
     
     Note:
     The cluster means may be used to create cut-off scores (i.e. the midpoint between
-    cluster means) in order to predict cluster membership of other subjects. The index
-    of topfeats may be used to constrain which regions are used as they provide the
-    most meaningful values regarding PIB status.
+    cluster means) in order to predict cluster membership of other subjects. Only a
+    subset of features is returned in order to constrain the regions used, as they 
+    provide the most meaningful values regarding PIB status.
     """
     # Select features accounting for up to 50% of the weighting
     sorted_weights = weights.sort(columns=0, ascending=False)
@@ -175,8 +174,8 @@ def calc_cutoffs(data, weights, clusters):
     topfeats = sum_weights[sum_weights[0] <= .50].index
     clust1subs = clusters[clusters[0] == 1].index
     clust2subs = clusters[clusters[0] == 2].index
-    clust1dat = sampdat.reindex(index=clust1subs, columns=topfeats)
-    clust2dat = sampdat.reindex(index=clust2subs, columns=topfeats)
+    clust1dat = data.reindex(index=clust1subs, columns=topfeats)
+    clust2dat = data.reindex(index=clust2subs, columns=topfeats)
     clust1mean = clust1dat.mean(axis=0)
     clust2mean = clust2dat.mean(axis=0)
     if clust1mean.mean() > clust2mean.mean():
@@ -188,6 +187,7 @@ def calc_cutoffs(data, weights, clusters):
         neg_means = clust1mean
         clust_renamed = clusters[0].astype(str).replace(['1','2'], ['neg','pos'])
     cutoffs = (pos_means + neg_means) / 2
+    clust_renamed.name = 'PIB_Status'
     return clust_renamed, cutoffs
 
 
@@ -198,9 +198,9 @@ def predict_clust(data, cutoffs):
     
     Inputs:
     -------------
-    data:   nxp dataframe where n is observations and p is features (i.e. ROIs)
-            Should be a pandas DataFrame with subject codes as index and only
-            features to be used in clustering as columns.
+    data: nxp dataframe where n is observations and p is features (i.e. ROIs)
+            Should be a pandas DataFrame with subject codes as index and features 
+            as columns.
             
     cutoffs:   pandas DataFrame   
                 index = features of interest labels, values = weights 
@@ -215,30 +215,104 @@ def predict_clust(data, cutoffs):
     # Classify as pos if any feature is above cutoff
     cutdata_agg = cutdata.any(axis=1)
     predicted_clust = cutdata_agg.astype(str).replace(['T', 'F'], ['pos', 'neg'])
+    predicted_clust.name = 'PIB_Status'
     return predicted_clust
-
+    
+    
+def create_tighclust(clusterdata):
+    """
+    Creates tight clusters consisting of subjects classified as a member of given 
+    cluster in at least 96% of resample runs.
+    Inputs:
+    ------------
+    clusterdata: Dataframe containing cluster membership over all resamples
+                    index = subject code 
+                    columns = resample run 
+                    values = cluster membership
+    Returns:
+    ------------
+    tight_subs: Dataframe containing only subjects belonging to tight clusters
+                and their cluster membership
+    """
+        
+    clust_totals = clusterdata.apply(pd.value_counts, axis=1)
+    clust_pct = clust_totals / len(clusterdata.columns)
+    pos_subs = clust_pct.index[clust_pct['pos'] > .96]
+    neg_subs = clust_pct.index[clust_pct['neg'] > .96]
+    tight_subs = pd.DataFrame(index=pos_subs + neg_subs,columns=pd.Index([0]))
+    tight_subs[0].ix[pos_subs] = 1
+    tight_subs[0].ix[neg_subs] = 2
+    return tight_subs
+    
+    
 def main(infile, outdir, visit):
+    """
+    Function to run full script. Takes input from command line and runs 
+    sparse k-means clustering with resampling to produce tight clusters.
+    Regional cutoffs are derived from these tight clusters and subjects
+    are classified based on these cutoffs.
+    """
+    
     # Select one visit, clean subj codes
     dataframe = prepare_data(infile, visit) 
     # Create empty frames to hold results of 
     weight_rslts, clust_rslts = create_rslts_frame(dataframe) 
-    for resamp_run in range(10):
+    for resamp_run in range(1000):
         # Get random sub-sample (without replacement) of group to feed into clustering
         # Currently set to 70% of group N
         sampdat, unsampdat = sample_data(dataframe)
         bestw, lowestw = skm_permute(sampdat)
         km_weight, km_clust = skm_cluster(sampdat, lowestw)      
-        samp_clust, cutoffs = calc_cutoffs(sampdat, km_weight, km_clust)
-        unsamp_clust = predict_clust(unsampdat, cutoffs)
+        samp_clust, sampcutoffs = calc_cutoffs(sampdat, km_weight, km_clust)
+        unsamp_clust = predict_clust(unsampdat, sampcutoffs)
         # Log weights and cluster membership of resample run
         weight_rslts[resamp_run] = km_weight[0]
         clust_rslts[resamp_run] = pd.concat([samp_clust, unsamp_clust])
+    # Create tight clusters and generate regional cut-offs to predict remaining subjects
+    tight_subs = create_tighclust(clust_rslts)
+    tight_clust, grpcutoffs = calc_cutoffs(dataframe.ix[tight_subs.index], 
+                                            pd.DataFrame(weight_rslts.mean(axis=1)), 
+                                            tight_subs)
+    untight_clust = predict_clust(dataframe.ix[dataframe.index - tight_clust.index],
+                                    grpcutoffs)
+    all_clust = pd.concat([tight_clust, untight_clust])
+
+    """
+    Or use the following?:
+    all_clust = predict_clust(dataframe, grpcutoffs) 
+    
+    Original version keeps members of tight clusters in these clusters. May rely more on 
+    how clustering classified them rather than regional cut-offs. Proposed version classifies
+    all subjects based on regional cut-offs. This may cause a subject that is part of one
+    tight cluster to be classified as part of the other cluster.
+    """
+    # Save out results of cluster membership and feature weights
+    all_clustout = os.path.join(outdir, 'ClusterResults.csv')
+    all_clust.to_csv(all_clustout, index=True, 
+                        index_label='SUBID', header=True,sep='\t')
+    print 'Cluster membership results saved to %s'%(all_clustout)
+    weight_totals = weight_rslts.mean(axis=1)
+    weight_totals.name = 'WeightMean'
+    weight_totalsout = os.path.join(outdir, 'FeatureWeights.csv')
+    weight_totals.to_csv(weight_totalsout, index=True, 
+                            index_label='Feature', header=True,sep='\t')
+    print 'Mean feature weights saved to %s'%(weight_totalsout)
+        
+##########################################################################
         
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description = """Sparse K-means Clustering with re-sampling to cluster subjects into PIB+ and PIB- groups.""",
+    parser = argparse.ArgumentParser(description = """
+    Sparse K-means Clustering with re-sampling to cluster subjects into 
+    PIB+ and PIB- groups.
+    -------------------------------------------------------------------""",
                                     formatter_class=argparse.RawTextHelpFormatter,
-                                    epilog= """Format of infile should be a spreadsheet with first column containing subject ID's and the remaining columns corresponding to ROIs to be used as features in clustering. The first row may contain headers.""")
+                                    epilog= """
+    -------------------------------------------------------------------
+    NOTE: Format of infile should be a spreadsheet with first column 
+    containing subject codes and the remaining columns corresponding to 
+    ROIs to be used as features in clustering. The first row may contain 
+    headers.""")
 
     parser.add_argument('infile', type=str, nargs=1,
                         help="""Input file containing subject codes and PIB indices""")
@@ -258,66 +332,7 @@ if __name__ == '__main__':
             args.visit = 'last'
             
         ### Begin running SKM clustering and resampling  
-        main(args.infile, args.outdir, args.visit)
+        main(args.infile[0], args.outdir, args.visit)
 
 
-"""
-OLD CODE BELOW, FOR US AS REFERENCE ONLY
-"""
-    # Load input spreadsheet into dataframe and select visit if multiple exist
-    datclean = prepare_data(infile)
-    datframe = pd.read_csv(infile, sep=None)
-    # Strip visit id (i.e. '_v1') from subject codes if they exist.
-    # Take most recent visit if multiple are included.
-    datclean = select_visit(datframe)
-    
-    
-    subjects = datframe.index
-    features = datframe.columns
-    
-    resamp_weights = pd.DataFrame(data=None, columns = features)
-    resamp_clusters = pd.DataFrame(data=None, columns = subjects)
-    
-    for resamp_run in range(10):
-        # Get random sub-sample (without replacement) of group to feed into clustering
-        # Currently set to 70% of group N
-        sampdat = subsample_data(datframe)
 
-        # Convert sampled data to R dataframe and run skm clustering
-        r_sampdat = com.convert_to_r_dataframe(sampdat)
-        km_wghtnorm, km_clust = skm_cluster(r_sampdat)
-        
-        # Find cluster centers for each group
-        clust1_means, clust2_means = clust_centers(sampdat, km_clust)
-
-        # Predict cluster membership for remaining subjects
-        """
-        for subj in subjects:
-            if subj not in sampdat.index:
-                compare subj data to clust1_means and clust2_means
-                or
-                if any region surpasses cut-off, then PIB+
-        """          
-        
-                
-        # Append results of resample to results arrays
-        resamp_weights = resamp_weights.append(km_wghtnorm, ignore_index=True)    
-        resamp_clusters = resamp_clusters.append(km_clust, ignore_index=True)
-    
-
-   
-"""
-TO DO:
---------
-1. Clean up input file. Check for subject id's with '_v1' etc. If they exist, 
-take most recent scan and strip visit indicator.
-2. Check conversion from dataframe to R. doesn't seem to match up.
-3. Predict cluster membership of remaining 30% of subjects and 
-save to results data arrays
-4. Construct tight clusters using subjects assigned to a cluster 
-in >=96% of samples
-5. Mid-point of these clusters becomes cut-off, a subject with 
-PIB index above this cut-off in any cluster is considered PIB+
-""" 
-    
-    
