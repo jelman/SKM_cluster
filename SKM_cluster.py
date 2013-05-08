e14b8d6fbf1c7d06a87025a117e9ff58c7c87ea0
jelman <jelman@lbl.gov>
2013-05-08 16:05:33 -0700

    Remove data cleaning function, add cmdline flags, change final cluster classification method

--- SKM_cluster.py
+++ SKM_cluster.py
@@ -4,38 +4,11 @@ import pandas as pd
 import pandas.rpy.common as com
 import rpy2
 import random
-sparcl = rpy2.robjects.packages.importr("sparcl")
+sparcl_pth = os.path.join(os.getenv('HOME'), 'R/packages')
+sparcl = rpy2.robjects.packages.importr("sparcl", lib_loc=sparcl_pth)
 
 
-def prepare_data(infile, visit='last'):
-    """
-    Takes input spreadsheet and loads into pd DataFrame. Assumes first 
-    column contains subject codes. If multiple visits exist, selects indicated 
-    datapoint (defaults to last). Returns DataFrame with subject codes as index.
-    
-    NOTE: Currently, can only determine timepoint if subject codes contain visit 
-    indicator (i.e. '_v1')
-    """
-    data = pd.read_csv(infile, sep=None)
-    subj_col = data.columns[0]    
-    # Check to see if subject codes contain visit indicator
-    if any(data.SUBID.str.contains('B*_v.')):
-        splitcol = data[subj_col].str.split('_')
-        data[subj_col] = splitcol.str[0]
-        data['Visit'] = splitcol.str[1]
-        # Sort data by subject code and visit number
-        sorteddata = data.sort(columns=[subj_col, 'Visit'])       
-        # Select either first or last visit depending on args. default is last
-        if visit == 'first':
-            singlevisit = sorteddata.drop_duplicates(cols=subj_col, take_last=False)
-        else:
-            singlevisit = sorteddata.drop_duplicates(cols=subj_col, take_last=True)           
-        singlevisit = singlevisit.drop('Visit', axis=1)
-        cleandata = singlevisit.set_index(subj_col)        
-    else:
-        singlevisit = sorteddata.drop_duplicates(cols=subj_col, take_last=True)
-        cleandata = singlevisit.set_index(subj_col)      
-    return cleandata
+
 
 
 def create_rslts_frame(dataframe):  
@@ -73,22 +46,25 @@ def skm_permute(data):
     
     Returns:
     ---------------
-    bestw: tuning parameter that returns the highest gap statistic
+    best_L1bound: float
+    	tuning parameter that returns the highest gap statistic
+	(more features given non-zero weights)
     
-    lowestw: smallest tuning parameter that gives a gap statistic within 
-                one sdgap of the largest gap statistic
+    lowest_L1bound: float
+    	smallest tuning parameter that gives a gap statistic within 
+        one sdgap of the largest gap statistic (sparser result)
     """
     r_data = com.convert_to_r_dataframe(data)
     km_perm = sparcl.KMeansSparseCluster_permute(r_data,K=2,nperms=25)
-    bestw = km_perm.rx2('bestw')[0]
+    best_L1bound = km_perm.rx2('best_L1bound')[0]
     wbounds = km_perm.rx2('wbounds')
     gaps = km_perm.rx2('gaps')
     bestgap = max(gaps)
     sdgaps = km_perm.rx2('sdgaps')
     # Calculate smallest wbound that returns gap stat within one sdgap of best wbound
     wbound_rnge = [wbounds[i] for i in range(len(gaps)) if (gaps[i]+sdgaps[i]>=bestgap)]
-    lowestw = min(wbound_rnge)
-    return bestw, lowestw
+    lowest_L1bound = min(wbound_rnge)
+    return best_L1bound, lowest_L1bound
     
     
 def skm_cluster(data, L1bound):
@@ -132,28 +108,32 @@ def skm_cluster(data, L1bound):
     return km_weightsnorm, km_clusters
         
         
-def calc_cutoffs(data, weights, clusters):
+def calc_cutoffs(data, weights, weightsum, clusters):
     """
-    Determines the features with the top 50% weights used in clustering and determines 
-    the cluster centers for these regions. Clusters are then labelled as positive or
-    negative based on which has the highest average PIB index across these features. 
-    This is done because clustering does not consistently assign the same label to groups
-    across runs. 
+    Determines the cluster centers for regions. This will only detmine cutoffs 
+    for features with weights making up the top percentile specified. Clusters 
+    are then labelled as positive or negative based on which has the highest 
+    average PIB index across these features. This is done because clustering 
+    does not consistently assign the same label to groups across runs. 
     
     Inputs
     ----------
-    data: nxp dataframe where n is observations and p is features (i.e. ROIs)
+    data:   pandas Dataframe
+            nxp dataframe where n is observations and p is features (i.e. ROIs)
             Should be a pandas DataFrame with subject codes as index and features 
             as columns.
             
-    weights:    pandas DataFrame   
+    weights:    pandas Dataframe   
                 index = feature labels, values = weights
-    clusters:   pandas DataFrame
+    weightsum:  float
+                cluster cut-offs will be determined for only for features with weihts
+                in the top percentile specified
+    clusters:   pandas Dataframe
                 index = subject code, values = cluster membership
     
     Returns:
     ----------
-    clust_renamed:  pandas DataFrame
+    clust_renamed:  pandas Dataframe
                     renamed version of clusters input such that integer labels
                     are replaced with appropriate string label ('pos' or 'neg')
     
@@ -163,15 +143,16 @@ def calc_cutoffs(data, weights, clusters):
                         for each feature
     
     Note:
-    The cluster means may be used to create cut-off scores (i.e. the midpoint between
-    cluster means) in order to predict cluster membership of other subjects. Only a
-    subset of features is returned in order to constrain the regions used, as they 
-    provide the most meaningful values regarding PIB status.
+    The cluster means may be used to create cut-off scores (i.e. the midpoint 
+    between cluster means) in order to predict cluster membership of other 
+    subjects. If a large number of features were used, it may be useful to 
+    set a lower weightsum (i.e., .50). This will constrain the features used
+    to classify subjects to only those that contributed most to clustering. 
     """
     # Select features accounting for up to 50% of the weighting
     sorted_weights = weights.sort(columns=0, ascending=False)
     sum_weights = sorted_weights.cumsum()
-    topfeats = sum_weights[sum_weights[0] <= .50].index
+    topfeats = sum_weights[sum_weights[0] <= weightsum].index
     clust1subs = clusters[clusters[0] == 1].index
     clust2subs = clusters[clusters[0] == 2].index
     clust1dat = data.reindex(index=clust1subs, columns=topfeats)
@@ -245,7 +226,7 @@ def create_tighclust(clusterdata):
     return tight_subs
     
     
-def main(infile, outdir, visit):
+def main(infile, outdir, nperms, weightsum, bound):
     """
     Function to run full script. Takes input from command line and runs 
     sparse k-means clustering with resampling to produce tight clusters.
@@ -253,18 +234,21 @@ def main(infile, outdir, visit):
     are classified based on these cutoffs.
     """
     
-    # Select one visit, clean subj codes
-    dataframe = prepare_data(infile, visit) 
+    # Load data to dataframe
+    dataframe = pd.read_csv(infile, sep=None, index_col=0)
     # Create empty frames to hold results of 
     weight_rslts, clust_rslts = create_rslts_frame(dataframe) 
-    for resamp_run in range(10):
+    for resamp_run in range(nperms):
         print 'Now starting re-sample run number %s'%(resamp_run)
         # Get random sub-sample (without replacement) of group to feed into clustering
         # Currently set to 70% of group N
         sampdat, unsampdat = sample_data(dataframe)
-        bestw, lowestw = skm_permute(sampdat)
-        km_weight, km_clust = skm_cluster(sampdat, lowestw)      
-        samp_clust, sampcutoffs = calc_cutoffs(sampdat, km_weight, km_clust)
+        best_L1bound, lowest_L1bound = skm_permute(sampdat)
+	if bound == 'sparse':
+            km_weight, km_clust = skm_cluster(sampdat, lowest_L1bound)      
+	else:
+	    km_weight, km_clust = skm_cluster(sampdat, best_L1bound)
+	samp_clust, sampcutoffs = calc_cutoffs(sampdat, km_weight, km_clust)
         unsamp_clust = predict_clust(unsampdat, sampcutoffs)
         # Log weights and cluster membership of resample run
         weight_rslts[resamp_run] = km_weight[0]
@@ -275,9 +259,7 @@ def main(infile, outdir, visit):
     tight_clust, grpcutoffs = calc_cutoffs(dataframe.ix[tight_subs.index], 
                                             pd.DataFrame(weight_totals), 
                                             tight_subs)
-    untight_subdata = dataframe.ix[dataframe.index - tight_clust.index]
-    untight_clust = predict_clust(untight_subdata, grpcutoffs)
-    all_clust = pd.concat([tight_clust, untight_clust])
+    all_clust = predict_clust(dataframe, grpcutoffs) 
     grpcutoffs.name = 'Cutoff_Value'
     weight_totals.name = 'WeightMean'
 
@@ -312,24 +294,33 @@ if __name__ == '__main__':
     headers.""")
 
     parser.add_argument('infile', type=str, nargs=1,
-                        help="""Input file containing subject codes and PIB indices""")
+            help='Input file containing subject codes and PIB indices')
     parser.add_argument('-outdir', dest='outdir', default=None,
-                        help="""Directory to save results. \ndefault = infile directory""")
-    parser.add_argument('-visit', type=str, dest='visit', default=None,
-                        choices=['first', 'last'],
-                        help="""Specify which visit to use if multiple visits exist \ndefault = last""")
-    
+            help='Directory to save results. (default = infile directory)')
+    parser.add_argument('-nperm', type=int, dest = 'nperm', default = 1000, 
+            help = 'Number of re-sample permutations (default 1000)')
+    parser.add_argument('-weightsum', type=float, dest = 'weightsum', 
+                        default = 1.0, 
+            help = """Only determine cutoffs for features with weights making 
+up the top percentile specified. Constrains the number 
+of features used for classification to those that were 
+most important in clustering (default = 1.0)""")
+    parser.add_argument('-bound', type=str, choices=['best', 'sparse'],
+                        dest='bound', default='best',
+            help="""method to determine L1bound, best result, or best  
+    sparse result""")
+	
+
     if len(sys.argv) == 1:
         parser.print_help()
     else:
         args = parser.parse_args()
         if args.outdir is None:
             args.outdir, _ = os.path.split(args.infile[0])
-        if args.visit is None:
-            args.visit = 'last'
+
             
         ### Begin running SKM clustering and resampling  
-        main(args.infile[0], args.outdir, args.visit)
+        main(args.infile[0], args.outdir, args.nperms, args.weightsum, args.bound)
 
 
 
